c759b5dae17045c2e9fb155ef67895362119395a
David S <99992909+david03130@users.noreply.github.com>
2023-04-28 17:26:40 +0200

    Diseño avanzado

--- Projectes/OnBoardTree/frmTree.Designer.cs
+++ Projectes/OnBoardTree/frmTree.Designer.cs
@@ -29,13 +29,50 @@ namespace OnBoardTree
         /// </summary>
         private void InitializeComponent()
         {
+            this.tree_Documents = new System.Windows.Forms.TreeView();
+            this.cmb_Collections = new System.Windows.Forms.ComboBox();
+            this.pnl_Details = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // tree_Documents
+            // 
+            this.tree_Documents.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tree_Documents.Location = new System.Drawing.Point(12, 46);
+            this.tree_Documents.Name = "tree_Documents";
+            this.tree_Documents.Size = new System.Drawing.Size(237, 501);
+            this.tree_Documents.TabIndex = 0;
+            // 
+            // cmb_Collections
+            // 
+            this.cmb_Collections.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.cmb_Collections.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Collections.FormattingEnabled = true;
+            this.cmb_Collections.Location = new System.Drawing.Point(12, 12);
+            this.cmb_Collections.Name = "cmb_Collections";
+            this.cmb_Collections.Size = new System.Drawing.Size(237, 24);
+            this.cmb_Collections.TabIndex = 1;
+            // 
+            // pnl_Details
+            // 
+            this.pnl_Details.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnl_Details.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnl_Details.Location = new System.Drawing.Point(268, 12);
+            this.pnl_Details.Name = "pnl_Details";
+            this.pnl_Details.Size = new System.Drawing.Size(808, 535);
+            this.pnl_Details.TabIndex = 2;
             // 
             // frmTree
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1088, 559);
+            this.Controls.Add(this.pnl_Details);
+            this.Controls.Add(this.cmb_Collections);
+            this.Controls.Add(this.tree_Documents);
             this.Name = "frmTree";
             this.Text = "OnBoard Detalles";
             this.ResumeLayout(false);
@@ -43,6 +80,10 @@ namespace OnBoardTree
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView tree_Documents;
+        private System.Windows.Forms.ComboBox cmb_Collections;
+        private System.Windows.Forms.Panel pnl_Details;
     }
 }
 
