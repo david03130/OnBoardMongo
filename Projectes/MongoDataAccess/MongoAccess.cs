ad6e5d1fe130d43b2d35c66772dfefc6d49f7db4
David S <99992909+david03130@users.noreply.github.com>
2023-04-28 17:34:05 +0200

    Proyecto MongoDataAccess creado

---
+++ Projectes/MongoDataAccess/MongoAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDataAccess
+{
+    public class MongoAccess<T>
+    {
+        //public IMongoCollection<> Collection { get; set; }
+
+        public T SelectAll()
+        {
+            List<T> items = 
+        }
+    }
+}
