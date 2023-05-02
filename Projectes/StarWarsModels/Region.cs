a4c453292fd31944c4012fd2f226913ac5aa104f
David S <99992909+david03130@users.noreply.github.com>
2023-05-02 17:09:05 +0200

    Region model created

---
+++ Projectes/StarWarsModels/Region.cs
@@ -0,0 +1,20 @@
+﻿using MongoDataAccess;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWarsModels
+{
+    public class Region : IMongoDataObject
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string nameRegion { get; set; }
+        public string descriptionRegion { get; set; }
+        public string mapRegion { get; set; }
+    }
+}
