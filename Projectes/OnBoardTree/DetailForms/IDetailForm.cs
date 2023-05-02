f223600093b9bba6cb8f775d807d7495d78c5341
David S <99992909+david03130@users.noreply.github.com>
2023-05-02 18:06:30 +0200

    IDetailForm created

---
+++ Projectes/OnBoardTree/DetailForms/IDetailForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBoardTree.DetailForms
+{
+    public interface IDetailForm
+    {
+        void LoadData(string id);
+    }
+}
