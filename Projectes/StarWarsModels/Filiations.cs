c23260040169cb3436dbfdc98c8846b4c041d3e4
David S <99992909+david03130@users.noreply.github.com>
2023-05-02 17:18:13 +0200

    All models have been created

---
+++ Projectes/StarWarsModels/Filiations.cs
@@ -0,0 +1,18 @@
+﻿using MongoDataAccess;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWarsModels
+{
+    public class Filiations : IMongoDataObject
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ Projectes/StarWarsModels/HyperspaceRoutes.cs
@@ -0,0 +1,22 @@
+﻿using MongoDataAccess;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWarsModels
+{
+    public class HyperspaceRoutes : IMongoDataObject
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string type { get; set; }
+        public string nameRoute { get; set; }
+        public string start { get; set; }
+        public string end { get; set; }
+    }
+
+}
