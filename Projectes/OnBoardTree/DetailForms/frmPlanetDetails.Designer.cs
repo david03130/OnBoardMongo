ca4ebc59f094bf5398a5192c7a9f6e05627408c1
David S <99992909+david03130@users.noreply.github.com>
2023-05-02 18:20:58 +0200

    frmPlanetDetails updated

--- Projectes/OnBoardTree/DetailForms/frmPlanetDetails.Designer.cs
+++ Projectes/OnBoardTree/DetailForms/frmPlanetDetails.Designer.cs
@@ -29,21 +29,48 @@ namespace OnBoardTree.DetailForms
         /// </summary>
         private void InitializeComponent()
         {
+            this.grp_GeneralInfo = new System.Windows.Forms.GroupBox();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.grp_GeneralInfo.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grp_GeneralInfo
+            // 
+            this.grp_GeneralInfo.Controls.Add(this.lbl_Name);
+            this.grp_GeneralInfo.Location = new System.Drawing.Point(12, 12);
+            this.grp_GeneralInfo.Name = "grp_GeneralInfo";
+            this.grp_GeneralInfo.Size = new System.Drawing.Size(262, 193);
+            this.grp_GeneralInfo.TabIndex = 0;
+            this.grp_GeneralInfo.TabStop = false;
+            this.grp_GeneralInfo.Text = "General Info";
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Location = new System.Drawing.Point(18, 34);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Name.TabIndex = 0;
+            this.lbl_Name.Text = "Name";
             // 
             // frmPlanetDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(642, 505);
+            this.Controls.Add(this.grp_GeneralInfo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmPlanetDetails";
             this.Text = "frmPlanetDetails";
+            this.grp_GeneralInfo.ResumeLayout(false);
+            this.grp_GeneralInfo.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grp_GeneralInfo;
+        private System.Windows.Forms.Label lbl_Name;
     }
 }
--- Projectes/OnBoardTree/DetailForms/frmPlanetDetails.cs
+++ Projectes/OnBoardTree/DetailForms/frmPlanetDetails.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MongoDataAccess;
+using StarWarsModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -10,11 +13,27 @@ using System.Windows.Forms;
 
 namespace OnBoardTree.DetailForms
 {
-    public partial class frmPlanetDetails : Form
+    public partial class frmPlanetDetails : Form, IDetailForm
     {
+        private readonly MongoDbConfig dbConfig;
+        private readonly MongoAccess<Planet> bbdd;
+
         public frmPlanetDetails()
         {
             InitializeComponent();
+
+            string dbUrl = ConfigurationManager.AppSettings["MongoConnectionString"].ToString();
+            string dbName = ConfigurationManager.AppSettings["MongoDataBaseName"].ToString();
+            dbConfig = new MongoDbConfig(dbUrl, dbName);
+            bbdd = new MongoAccess<Planet>(dbConfig, "Planets");
+        }
+
+        public void LoadData(string id)
+        {
+            // Carregar les dades a partir del id.
+            Planet planet = bbdd.SelectById(id);
+
+            lbl_Name.Text = planet.name;
         }
     }
 }
