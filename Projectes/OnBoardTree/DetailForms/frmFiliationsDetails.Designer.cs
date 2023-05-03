21deed6de7d7961f0e07556d5d4e9e6a631b7b6a
David S <99992909+david03130@users.noreply.github.com>
2023-05-03 16:07:48 +0200

    frmFiliationDetails design updated

--- Projectes/OnBoardTree/DetailForms/frmFiliationsDetails.Designer.cs
+++ Projectes/OnBoardTree/DetailForms/frmFiliationsDetails.Designer.cs
@@ -37,13 +37,11 @@ namespace OnBoardTree.DetailForms
             // 
             // grp_GeneralInfo
             // 
-            this.grp_GeneralInfo.Controls.Add(this.lbl_DescriptionTitle);
             this.grp_GeneralInfo.Controls.Add(this.lbl_Description);
-            this.grp_GeneralInfo.Location = new System.Drawing.Point(16, 15);
-            this.grp_GeneralInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grp_GeneralInfo.Controls.Add(this.lbl_DescriptionTitle);
+            this.grp_GeneralInfo.Location = new System.Drawing.Point(12, 12);
             this.grp_GeneralInfo.Name = "grp_GeneralInfo";
-            this.grp_GeneralInfo.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.grp_GeneralInfo.Size = new System.Drawing.Size(737, 351);
+            this.grp_GeneralInfo.Size = new System.Drawing.Size(290, 106);
             this.grp_GeneralInfo.TabIndex = 0;
             this.grp_GeneralInfo.TabStop = false;
             this.grp_GeneralInfo.Text = "General Info";
@@ -52,10 +50,9 @@ namespace OnBoardTree.DetailForms
             // 
             this.lbl_DescriptionTitle.AutoSize = true;
             this.lbl_DescriptionTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_DescriptionTitle.Location = new System.Drawing.Point(17, 36);
-            this.lbl_DescriptionTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_DescriptionTitle.Location = new System.Drawing.Point(13, 29);
             this.lbl_DescriptionTitle.Name = "lbl_DescriptionTitle";
-            this.lbl_DescriptionTitle.Size = new System.Drawing.Size(100, 20);
+            this.lbl_DescriptionTitle.Size = new System.Drawing.Size(79, 16);
             this.lbl_DescriptionTitle.TabIndex = 3;
             this.lbl_DescriptionTitle.Text = "Description:";
             // 
@@ -63,21 +60,19 @@ namespace OnBoardTree.DetailForms
             // 
             this.lbl_Description.AutoSize = true;
             this.lbl_Description.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Description.Location = new System.Drawing.Point(153, 36);
-            this.lbl_Description.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Description.Location = new System.Drawing.Point(130, 29);
             this.lbl_Description.Name = "lbl_Description";
-            this.lbl_Description.Size = new System.Drawing.Size(95, 20);
-            this.lbl_Description.TabIndex = 2;
+            this.lbl_Description.Size = new System.Drawing.Size(76, 16);
+            this.lbl_Description.TabIndex = 4;
             this.lbl_Description.Text = "Description";
             // 
             // frmFiliationsDetails
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(769, 622);
+            this.ClientSize = new System.Drawing.Size(337, 144);
             this.Controls.Add(this.grp_GeneralInfo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmFiliationsDetails";
             this.Text = "frmPlanetDetails";
             this.grp_GeneralInfo.ResumeLayout(false);
