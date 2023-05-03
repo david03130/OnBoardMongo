5a01f30d02dd6eb99e1fa4051e41e92325268e68
David S <99992909+david03130@users.noreply.github.com>
2023-05-03 16:08:36 +0200

    frmRegionsDetails design updated

--- Projectes/OnBoardTree/DetailForms/frmRegionsDetails.Designer.cs
+++ Projectes/OnBoardTree/DetailForms/frmRegionsDetails.Designer.cs
@@ -31,21 +31,25 @@ namespace OnBoardTree.DetailForms
         {
             this.grp_GeneralInfo = new System.Windows.Forms.GroupBox();
             this.lbl_DescriptionTitle = new System.Windows.Forms.Label();
-            this.lbl_Description = new System.Windows.Forms.Label();
             this.lbl_NameTitle = new System.Windows.Forms.Label();
             this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_Description = new System.Windows.Forms.Label();
+            this.pic_Map = new System.Windows.Forms.PictureBox();
             this.grp_GeneralInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Map)).BeginInit();
             this.SuspendLayout();
             // 
             // grp_GeneralInfo
             // 
-            this.grp_GeneralInfo.Controls.Add(this.lbl_DescriptionTitle);
+            this.grp_GeneralInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.grp_GeneralInfo.Controls.Add(this.lbl_Description);
+            this.grp_GeneralInfo.Controls.Add(this.lbl_DescriptionTitle);
             this.grp_GeneralInfo.Controls.Add(this.lbl_NameTitle);
             this.grp_GeneralInfo.Controls.Add(this.lbl_Name);
             this.grp_GeneralInfo.Location = new System.Drawing.Point(12, 12);
             this.grp_GeneralInfo.Name = "grp_GeneralInfo";
-            this.grp_GeneralInfo.Size = new System.Drawing.Size(553, 285);
+            this.grp_GeneralInfo.Size = new System.Drawing.Size(604, 232);
             this.grp_GeneralInfo.TabIndex = 0;
             this.grp_GeneralInfo.TabStop = false;
             this.grp_GeneralInfo.Text = "General Info";
@@ -59,16 +63,6 @@ namespace OnBoardTree.DetailForms
             this.lbl_DescriptionTitle.Size = new System.Drawing.Size(79, 16);
             this.lbl_DescriptionTitle.TabIndex = 3;
             this.lbl_DescriptionTitle.Text = "Description:";
-            // 
-            // lbl_Description
-            // 
-            this.lbl_Description.AutoSize = true;
-            this.lbl_Description.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Description.Location = new System.Drawing.Point(115, 63);
-            this.lbl_Description.Name = "lbl_Description";
-            this.lbl_Description.Size = new System.Drawing.Size(76, 16);
-            this.lbl_Description.TabIndex = 2;
-            this.lbl_Description.Text = "Description";
             // 
             // lbl_NameTitle
             // 
@@ -90,17 +84,44 @@ namespace OnBoardTree.DetailForms
             this.lbl_Name.TabIndex = 0;
             this.lbl_Name.Text = "Name";
             // 
+            // lbl_Description
+            // 
+            this.lbl_Description.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_Description.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Description.Location = new System.Drawing.Point(13, 88);
+            this.lbl_Description.Name = "lbl_Description";
+            this.lbl_Description.Size = new System.Drawing.Size(578, 130);
+            this.lbl_Description.TabIndex = 4;
+            this.lbl_Description.Text = "Description";
+            // 
+            // pic_Map
+            // 
+            this.pic_Map.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic_Map.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pic_Map.Location = new System.Drawing.Point(41, 261);
+            this.pic_Map.Name = "pic_Map";
+            this.pic_Map.Size = new System.Drawing.Size(539, 317);
+            this.pic_Map.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_Map.TabIndex = 1;
+            this.pic_Map.TabStop = false;
+            // 
             // frmRegionsDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 505);
+            this.ClientSize = new System.Drawing.Size(628, 600);
+            this.Controls.Add(this.pic_Map);
             this.Controls.Add(this.grp_GeneralInfo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmRegionsDetails";
             this.Text = "frmPlanetDetails";
             this.grp_GeneralInfo.ResumeLayout(false);
             this.grp_GeneralInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Map)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -110,7 +131,8 @@ namespace OnBoardTree.DetailForms
         private System.Windows.Forms.GroupBox grp_GeneralInfo;
         private System.Windows.Forms.Label lbl_Name;
         private System.Windows.Forms.Label lbl_DescriptionTitle;
-        private System.Windows.Forms.Label lbl_Description;
         private System.Windows.Forms.Label lbl_NameTitle;
+        private System.Windows.Forms.Label lbl_Description;
+        private System.Windows.Forms.PictureBox pic_Map;
     }
 }
